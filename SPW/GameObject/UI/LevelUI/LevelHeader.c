36d8c78dff710a6adb43a9c4f315437bfbb2b539
Gabriel NIERAT <fareanor.kelfasquin@gmail.com>
2022-06-09 10:18:16 +0200

    print life correctly

--- SPW/GameObject/UI/LevelUI/LevelHeader.c
+++ SPW/GameObject/UI/LevelUI/LevelHeader.c
@@ -63,13 +63,17 @@ void LevelHeader_Constructor(void *self, void *scene)
     RE_Atlas *uiAtlas = AssetManager_GetUIAtlas(assets);
     RE_AtlasPart *part = RE_Atlas_GetPart(uiAtlas, "Firefly");
     RE_AtlasPart* Hpart = RE_Atlas_GetPart(uiAtlas, "Heart");
+    RE_AtlasPart* Lpart = RE_Atlas_GetPart(uiAtlas, "Life");
     AssertNew(part);
     AssertNew(Hpart);
+	AssertNew(Lpart);
 
     Image *fireflyImg = Scene_AllocateObject(scene, Class_Image);
     AssertNew(fireflyImg);
     Image* heartImg = Scene_AllocateObject(scene, Class_Image);
     AssertNew(heartImg);
+	Image* lifeImg = Scene_AllocateObject(scene, Class_Image);
+	AssertNew(lifeImg);
 
     Image_Constructor(fireflyImg, scene, part, 0);
     rect.m_anchorMin = Vec2_Set(0.0f, 0.0f);
@@ -80,12 +84,20 @@ void LevelHeader_Constructor(void *self, void *scene)
     GameObject_SetParent(fireflyImg, panel);
 
     Image_Constructor(heartImg, scene, Hpart, 0);
+    rect.m_anchorMin = Vec2_Set(0.0f, 2.0f);
+    rect.m_anchorMax = Vec2_Set(0.0f, 2.0f);
+    rect.m_offsetMin = Vec2_Set(0.0f, 2.0f);
+    rect.m_offsetMax = Vec2_Set(imgW, imgH);
+    UIObject_SetLocalRect(heartImg, rect);
+    GameObject_SetParent(heartImg, panel);
+
+    Image_Constructor(lifeImg, scene, Lpart, 0);
     rect.m_anchorMin = Vec2_Set(0.0f, 1.0f);
     rect.m_anchorMax = Vec2_Set(0.0f, 1.0f);
     rect.m_offsetMin = Vec2_Set(0.0f, 1.0f);
     rect.m_offsetMax = Vec2_Set(imgW, imgH);
-    UIObject_SetLocalRect(heartImg, rect);
-    GameObject_SetParent(heartImg, panel);
+    UIObject_SetLocalRect(lifeImg, rect);
+    GameObject_SetParent(lifeImg, panel);
 
     // Compteur du nombre de luciolles
     Text *fireflyCountText = Scene_AllocateObject(scene, Class_Text);
@@ -100,17 +112,31 @@ void LevelHeader_Constructor(void *self, void *scene)
     GameObject_SetParent(fireflyCountText, panel);
     panel->m_fireflyCountText = fireflyCountText;
 
+    //compteur du nombre de coeurs du joueur
     Text* heartCountText = Scene_AllocateObject(scene, Class_Text);
     AssertNew(heartCountText);
     Text_Constructor(heartCountText, scene, "2", font, normalColor);
     Text_SetAnchor(heartCountText, RE_ANCHOR_LEFT | RE_ANCHOR_MIDDLE);
-    rect.m_anchorMin = Vec2_Set(0.0f, 1.0f);
-    rect.m_anchorMax = Vec2_Set(0.0f, 1.0f);
-    rect.m_offsetMin = Vec2_Set(imgW + sep, 1.0f);
+    rect.m_anchorMin = Vec2_Set(0.0f, 2.0f);
+    rect.m_anchorMax = Vec2_Set(0.0f, 2.0f);
+    rect.m_offsetMin = Vec2_Set(imgW + sep, 2.0f);
     rect.m_offsetMax = Vec2_Set(imgW + sep + numW, imgH);
     UIObject_SetLocalRect(heartCountText, rect);
     GameObject_SetParent(heartCountText, panel);
     panel->m_heartCountText = heartCountText;
+	
+    //compteur du nombre de vies du joueur
+    Text* lifeCountText = Scene_AllocateObject(scene, Class_Text);
+    AssertNew(lifeCountText);
+    Text_Constructor(lifeCountText, scene, "5", font, normalColor);
+    Text_SetAnchor(lifeCountText, RE_ANCHOR_LEFT | RE_ANCHOR_MIDDLE);
+    rect.m_anchorMin = Vec2_Set(0.0f, 1.0f);
+    rect.m_anchorMax = Vec2_Set(0.0f, 1.0f);
+    rect.m_offsetMin = Vec2_Set(imgW + sep, 1.0f);
+    rect.m_offsetMax = Vec2_Set(imgW + sep + numW, imgH);
+    UIObject_SetLocalRect(lifeCountText, rect);
+    GameObject_SetParent(lifeCountText, panel);
+    panel->m_lifeCountText = lifeCountText;
 }
 
 void LevelHeader_VM_Destructor(void *self)
@@ -131,6 +157,8 @@ void LevelHeader_VM_Update(void *self)
     int count = 0;
     char strH[128] = { 0 };
     int countH = 0;
+    char strL[128] = { 0 };
+    int countL = 0;
 
     count = Player_GetFireflyCount(player);
     sprintf(str, "%d", count);
@@ -140,4 +168,7 @@ void LevelHeader_VM_Update(void *self)
     countH = Player_GetHeartCount(player);
     sprintf(strH, "%d", countH);
     Text_SetString(panel->m_heartCountText, strH);
+    countL = Player_GetLifeCount(player);
+    sprintf(strL, "%d", countL);
+    Text_SetString(panel->m_lifeCountText, strL);
 }
